1a48c7c40855aac6d630317c29511490f4278d8c
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-24 14:18:32 +0100

    Exo2 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tempMin = -20;
+const tempMax = 40;
+const heartMin = 80;
+const heartMax = 180;
+const stepMin = 0;
+const stepMax = 50000;
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container-fluid'>
+        <h1 className='text-primary'>Hello !</h1>
+        <div class='row'>
+        <p>Heart : {heartMin}</p>
+        <p>Temperature : {tempMin}</p>
+        <p>Steps : {stepMin}</p>
+
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
